b02a681079909a9ca0b296d00570311f4b8f9469
Aya Bouri <aya.bouri1@gmail.com>
2024-01-08 22:30:31 +0100

    projet

---
+++ src/app/projects/Models/projet.ts
@@ -0,0 +1,9 @@
+export class Projet {
+    Id:number=0;
+    Libelle:string="";
+    Description:string="";
+    SprintDays:number=0;
+    DateDebut!:Date;
+    DateFin!:Date;
+    /*"Sprints":null;*/
+}
