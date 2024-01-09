dbd60a6294b05bdee9b1a8e35d220c9b823a6923
Aya Bouri <aya.bouri1@gmail.com>
2024-01-09 08:50:46 +0100

    projet

--- src/app/projects/add/add.component.ts
+++ src/app/projects/add/add.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class AddComponent {
-
+export class AddComponent implements OnInit{
+  constructor(private http:HttpClient){
+    //this.http.get("https://www.mani-api.ma/api/Projets").subscribe();
+  }
+  ngOnInit(): void {
+  }
 }
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { AddComponent } from './add/add.component';
-
-
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AddComponent
   ],
   imports: [
     CommonModule,
-    ProjectsRoutingModule
+    ProjectsRoutingModule,
+    HttpClientModule
   ]
 })
 export class ProjectsModule { }
