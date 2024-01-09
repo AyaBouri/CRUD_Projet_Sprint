22da0c0d5fb6bf5cbb4ca995e97b4bc1a747f018
Aya Bouri <aya.bouri1@gmail.com>
2024-01-09 08:55:41 +0100

    projet

--- src/app/projects/add/add.component.ts
+++ src/app/projects/add/add.component.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ProjetsService } from '../Services/projets.service';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit{
-  constructor(private http:HttpClient){
+  constructor(private ProjetsService:ProjetsService){
     //this.http.get("https://www.mani-api.ma/api/Projets").subscribe();
   }
   ngOnInit(): void {
+    //this.http.get("https://www.mani-api.ma/api/Projets").subscribe();
   }
 }
