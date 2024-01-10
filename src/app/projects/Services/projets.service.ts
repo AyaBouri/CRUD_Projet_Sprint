514db7a938c619a366dd6014b884a2fc3e66a8f5
Aya Bouri <aya.bouri1@gmail.com>
2024-01-10 18:57:48 +0100

    projet

--- src/app/projects/Services/projets.service.ts
+++ src/app/projects/Services/projets.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Projet } from '../Models/projet';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: "any"
 })
 export class ProjetsService {
   url:string="https://www.mani-api.ma/api/Projets";
   constructor(private http:HttpClient) { }
-  getAll(){
-    return this.http.get(this.url);
+  getAll():Observable<Projet[]>{
+    return this.http.get<Projet[]>(this.url);
   }
   add(projet:Projet){
     return this.http.post(this.url,projet);
--- src/app/projects/list/list.component.ts
+++ src/app/projects/list/list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Projet } from '../Models/projet';
+import { ProjetsService } from '../Services/projets.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
-
+export class ListComponent implements OnInit{
+  projets:Projet[]=[];
+  constructor(private ProjetService:ProjetsService){}
+  ngOnInit(): void {
+    this.ProjetService.getAll().subscribe({
+      next:(data)=>{
+        this.projets=data;
+      },
+      error:()=>{},
+      complete:()=>{}
+    });
+  }
+  remove(id:number){
+    this.ProjetService
+  }
 }
