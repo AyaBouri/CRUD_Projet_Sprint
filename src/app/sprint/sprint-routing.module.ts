a4b45c73e97158458ac55ec32c156953e543292c
Aya Bouri <aya.bouri1@gmail.com>
2024-01-11 15:01:31 +0100

    projet

--- src/app/sprint/sprint-routing.module.ts
+++ src/app/sprint/sprint-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { AddComponent } from './add/add.component';
+const routes: Routes = [
+  {path:'add',component:AddComponent}
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/app/sprint/sprint.module.ts
+++ src/app/sprint/sprint.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SprintRoutingModule } from './sprint-routing.module';
+import { AddComponent } from './add/add.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddComponent
+  ],
   imports: [
     CommonModule,
     SprintRoutingModule
